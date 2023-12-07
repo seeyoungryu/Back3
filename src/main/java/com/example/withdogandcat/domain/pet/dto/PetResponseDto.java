4a273a0b51b495b6231e2f8a434dda0e5e341107
seeyoungryu <seeyoungryu@gmail.com>
2023-12-08 02:29:17 +0900

    update: pet등록 필드값 추가 (Dto 수정, Entity 수정, Enum 클래스 추가)

--- src/main/java/com/example/withdogandcat/domain/pet/dto/PetResponseDto.java
+++ src/main/java/com/example/withdogandcat/domain/pet/dto/PetResponseDto.java
@@ -9,24 +9,33 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PetResponseDto {
-    private Long id;
     private Long petId;
+    private String nickname;
     private String petName;
+    private String petGender;
+    private String petKind;
+    private String petInfo;
     private String imageUrl;
 
     @Builder
-    public PetResponseDto(Long id, Long petId, String petName, String imageUrl) {
-        this.id = id;
+    public PetResponseDto(Long petId, String nickname, String petName, String petGender, String petKind, String petInfo, String imageUrl) {
         this.petId = petId;
+        this.nickname = nickname;
         this.petName = petName;
+        this.petGender = petGender;
+        this.petKind = petKind;
+        this.petInfo = petInfo;
         this.imageUrl = imageUrl;
     }
 
     public static PetResponseDto from(Pet pet) {
         return PetResponseDto.builder()
-                .id(1L)
                 .petId(pet.getId())
+                .nickname(pet.getUser().getNickname())
                 .petName(pet.getName())
+                .petGender(pet.getGender().name())
+                .petKind(pet.getKind().name())
+                .petInfo(pet.getPetInfo())
                 .imageUrl(pet.getImageUrl())
                 .build();
     }
--- src/main/java/com/example/withdogandcat/domain/pet/entity/Pet.java
+++ src/main/java/com/example/withdogandcat/domain/pet/entity/Pet.java
@@ -1,5 +1,4 @@
 package com.example.withdogandcat.domain.pet.entity;
-
 import com.example.withdogandcat.domain.pet.dto.PetRequestDto;
 import com.example.withdogandcat.domain.user.entity.User;
 import jakarta.persistence.*;
@@ -8,12 +7,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-/**
- * 애완동물 엔티티 클래스
- */
 @Entity
 @Getter
-@Table
+@Table(name = "pets")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Pet {
 
@@ -24,30 +20,51 @@ public class Pet {
     @Column(nullable = false)
     private String name;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PetGender gender;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PetKind kind;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
     @Column(nullable = false)
     private String imageUrl;
+
     @Column(nullable = false)
     private String imageName;
 
+    @Column(length = 500) // petInfo 필드의 길이를 지정합니다.
+    private String petInfo;
+
     @Builder
-    public Pet(User user, String name, String imageUrl, String imageName) {
+    public Pet(User user, String name, PetGender petGender, PetKind petKind, String imageUrl, String imageName, String petInfo) {
+        this.user = user;
         this.name = name;
+        this.gender = petGender;
+        this.kind = petKind;
         this.imageUrl = imageUrl;
         this.imageName = imageName;
+        this.petInfo = petInfo;
     }
+
 
     public static Pet createPet(PetRequestDto requestDto, User user, String imageUrl, String imageName) {
         return Pet.builder()
-                .name(requestDto.getPetName())
                 .user(user)
+                .name(requestDto.getPetName())
                 .imageUrl(imageUrl)
                 .imageName(imageName)
+                .petInfo(requestDto.getPetInfo())
+                .petGender(requestDto.getPetGender())
+                .petKind(requestDto.getPetKind())
                 .build();
     }
+
 
     public void updateImage(String imageUrl, String originalFilename) {
         this.imageUrl = imageUrl;
@@ -57,5 +74,18 @@ public class Pet {
 
     public void updateName(String petName) {
         this.name = petName;
+    }
+
+
+    public void updatePetInfo(String petInfo) {
+        this.petInfo = petInfo;
+    }
+
+    public void updatePetGender(PetGender gender) {
+        this.gender = gender;
+    }
+
+    public void updatePetKind(PetKind kind) {
+        this.kind = kind;
     }
 }
