fc5c47a7d8c352f9cf68df70d753c306214cce2c
Hewllpark <hewllpark@gmail.com>
2023-12-29 14:21:10 +0900

    FIX: Map 기본 기능으로 수정 (재)

--- src/main/java/com/example/withdogandcat/domain/map/MapController.java
+++ src/main/java/com/example/withdogandcat/domain/map/MapController.java
@@ -1,39 +1,43 @@
 package com.example.withdogandcat.domain.map;
 
-import com.example.withdogandcat.domain.map.dto.MapShopRequestDto;
-import com.example.withdogandcat.domain.map.dto.MapShopResponseDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/shops")
+@RequestMapping("/api/map")
 public class MapController {
 
+    private static final Logger logger = LoggerFactory.getLogger(MapController.class);
+
+    private final GeoLocationService geoLocationService;
     private final MapService mapService;
 
     @Autowired
-    public MapController(MapService mapService) {
+    public MapController(GeoLocationService geoLocationService, MapService mapService) {
+        this.geoLocationService = geoLocationService;
         this.mapService = mapService;
     }
 
-    @PostMapping("/map")
-    public ResponseEntity<MapShopResponseDto> addLocation(@RequestBody MapShopRequestDto mapShopRequestDto) {
-        MapShopResponseDto mapShopResponseDto = mapService.addLocation(mapShopRequestDto.getShopId(), mapShopRequestDto);
-        return ResponseEntity.ok(mapShopResponseDto);
+    @GetMapping("")
+    public ResponseEntity<List<Place>> getSavedResults() {
+        List<Place> places = mapService.getAllPlaces();
+        return ResponseEntity.ok(places);
     }
 
-    @GetMapping("/{shopId}/map")
-    public ResponseEntity<MapShopResponseDto> getLocation(@PathVariable Long shopId) {
-        MapShopResponseDto mapShopResponseDto = mapService.getLocation(shopId);
-        return ResponseEntity.ok(mapShopResponseDto);
-    }
-
-    @PutMapping("/{shopId}/map")
-    public ResponseEntity<MapShopResponseDto> updateLocation(@PathVariable Long shopId, @RequestBody MapShopRequestDto mapShopRequestDto) {
-        MapShopResponseDto mapShopResponseDto = mapService.updateLocation(shopId, mapShopRequestDto);
-        return ResponseEntity.ok(mapShopResponseDto);
+    @PostMapping("")
+    public ResponseEntity<?> saveLocation(@RequestBody List<Place> places) {
+        try {
+            mapService.savePlaces(places);
+            logger.info("검색 결과를 저장했습니다.");
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            logger.error("검색 결과 저장 실패:", e);
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
--- src/main/java/com/example/withdogandcat/domain/map/MapService.java
+++ src/main/java/com/example/withdogandcat/domain/map/MapService.java
@@ -1,68 +1,30 @@
 package com.example.withdogandcat.domain.map;
 
-import com.example.withdogandcat.domain.map.dto.MapShopRequestDto;
-import com.example.withdogandcat.domain.map.dto.MapShopResponseDto;
-import com.example.withdogandcat.domain.shop.ShopRepository;
-import com.example.withdogandcat.domain.shop.entity.Shop;
-import com.example.withdogandcat.global.exception.BaseException;
-import com.example.withdogandcat.global.exception.BaseResponseStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 
 @Service
 public class MapService {
 
-    private final MapShopRepository mapShopRepository;
-    private final ShopRepository shopRepository;
+    private final PlaceRepository placeRepository;
 
     @Autowired
-    public MapService(MapShopRepository mapShopRepository, ShopRepository shopRepository) {
-        this.mapShopRepository = mapShopRepository;
-        this.shopRepository = shopRepository;
+    public MapService(PlaceRepository placeRepository) {
+        this.placeRepository = placeRepository;
     }
 
     @Transactional
-    public MapShopResponseDto addLocation(Long shopId, MapShopRequestDto mapShopRequestDto) {
-        Shop shop = shopRepository.findById(shopId)
-                .orElseThrow(() -> new BaseException(BaseResponseStatus.SHOP_NOT_FOUND));
-
-        MapShop mapShop = MapShop.builder()
-                .address(mapShopRequestDto.getAddress())
-                .latitude(mapShopRequestDto.getLatitude())
-                .longitude(mapShopRequestDto.getLongitude())
-                .build();
-
-        mapShopRepository.save(mapShop);
-
-        return new MapShopResponseDto(mapShop);
+    public void savePlaces(List<Place> places) {
+        for (Place place : places) {
+            placeRepository.save(place);
+        }
     }
 
     @Transactional(readOnly = true)
-    public MapShopResponseDto getLocation(Long shopId) {
-        Shop shop = shopRepository.findById(shopId)
-                .orElseThrow(() -> new BaseException(BaseResponseStatus.SHOP_NOT_FOUND));
-
-        MapShop mapShop = mapShopRepository.findByShop(shop)
-                .orElseThrow(() -> new BaseException(BaseResponseStatus.MAP_LOCATION_NOT_FOUND));
-
-        return new MapShopResponseDto(mapShop);
+    public List<Place> getAllPlaces() {
+        return placeRepository.findAll();
     }
-
-
-    @Transactional
-    public MapShopResponseDto updateLocation(Long shopId, MapShopRequestDto requestDto) {
-        Shop shop = shopRepository.findById(shopId)
-                .orElseThrow(() -> new BaseException(BaseResponseStatus.SHOP_NOT_FOUND));
-
-        MapShop mapShop = mapShopRepository.findByShop(shop)
-                .orElseThrow(() -> new BaseException(BaseResponseStatus.MAP_LOCATION_NOT_FOUND));
-
-        mapShop.updateLocation(requestDto.getAddress(), requestDto.getLatitude(), requestDto.getLongitude());
-        mapShopRepository.save(mapShop);
-
-        return new MapShopResponseDto(mapShop);
-    }
-
 }
