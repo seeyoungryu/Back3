c9100ce1623dfebc97ddaf3b9fdcbeb4330b630c
Hewllpark <hewllpark@gmail.com>
2024-01-04 21:55:43 +0900

    FIX: 테스트 서버 ip추가

--- src/main/java/com/example/withdogandcat/global/config/WebConfig.java
+++ src/main/java/com/example/withdogandcat/global/config/WebConfig.java
@@ -14,13 +14,14 @@ public class WebConfig implements WebMvcConfigurer {
     private static final String PRODUCTION_HOST = "http://3.37.121.136:8080";
     private static final String PRODUCTION_HOST2 = "http://3.37.121.136:443";
     private static final String PRODUCTION_HOST3 = "https://warrwarr.co.kr";
+    private static final String PRODUCTION_HOST4 = "http://52.78.102.94:8080";
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowCredentials(true)
                 .allowedOrigins(REACT_LOCAL_HOST, REACT_LOCAL_HOST2, REACT_LOCAL_HOST3,
-                        PRODUCTION_HOST, PRODUCTION_HOST2, PRODUCTION_HOST3)
+                        PRODUCTION_HOST, PRODUCTION_HOST2, PRODUCTION_HOST3, PRODUCTION_HOST4)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization");
