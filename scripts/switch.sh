b5252885ac0c8d55f9aeec9671f767495c8ea71b
Hewllpark <hewllpark@gmail.com>
2024-01-05 20:53:24 +0900

    switch 스크립트 수정

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-CURRENT_PORT=$(cat /home/ec2-user/service_url.inc | grep -Po '[0-9]+' | tail -1)
+CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
+
 TARGET_PORT=0
 
 echo "> Nginx currently proxies to ${CURRENT_PORT}."
@@ -14,4 +15,4 @@ else
     exit 1
 fi
 
-echo "set \$service_url http://3.37.121.136:${TARGET_PORT};" | tee /home/ubuntu/service_url.inc
+echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /home/ubuntu/service_url.inc
